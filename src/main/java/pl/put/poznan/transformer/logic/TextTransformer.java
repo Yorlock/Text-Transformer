467868d9b8ebc5050dfb86dd3f74e73549effa18
Anukia <59363243+Anukia@users.noreply.github.com>
2021-12-18 17:42:25 +0100

    Added upper, lower and capitalize function in TextTransformer.java

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformer.java
@@ -1,5 +1,7 @@
 package pl.put.poznan.transformer.logic;
 
+import org.apache.commons.lang.WordUtils;
+
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
@@ -12,7 +14,55 @@ public class TextTransformer {
     }
 
     public String transform(String text){
-        // of course, normally it would do something based on the transforms
+        String result = text;
+        for(String transform: transforms) {
+            switch (transform) {
+                case "upper":
+                    result = upper(text);
+                    break;
+                case "lower":
+                    result = lower(text);
+                    break;
+                case "capitalize":
+                    result = capitalize(text);
+                    break;
+                case "inverse":
+                    result = inverse(text);
+                    break;
+                case "intToWord":
+                    result = intToWord(text);
+                    break;
+                case "repeats":
+                    result = repeats(text);
+                    break;
+
+            }
+        }
+        return result;
+    }
+
+    public String upper(String text) {
         return text.toUpperCase();
     }
+
+    public String lower (String text) {
+        return text.toLowerCase();
+    }
+
+    public String capitalize (String text) {
+        return WordUtils.capitalizeFully(text);
+    }
+
+    public String inverse(String text) {
+        return null;
+    }
+
+    public String intToWord(String text) {
+        return null;
+    }
+
+    public String repeats (String text) {
+        return null;
+    }
+
 }
