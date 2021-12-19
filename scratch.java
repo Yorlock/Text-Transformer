7e87528fa26ac1914e81bab934272ee114674f5e
AlistarIsCow <62110503+AlistarIsCow@users.noreply.github.com>
2021-12-19 11:39:16 +0100

    Update scratch.java

--- scratch.java
+++ scratch.java
@@ -1,8 +1,25 @@
+package scratch;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-class Scratch {
-    public static String capitalize(String text) {
-        char character;
+public class main {
+	
+	public static boolean isFloat(String string) {
+		if (string == null) {
+			return false;
+		}
+		try {
+			float f = Float.parseFloat(string);
+		}
+		catch(NumberFormatException nfe)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public static String capitalize(String text) {
         String result = "";
         for (int i=0; i<text.length(); i++)
         {
@@ -26,9 +43,8 @@ class Scratch {
     }
 
     public static String inverse(String text) {
-        char character;
         String result = "";
-        ArrayList<Boolean> uppers = new ArrayList<Boolean>();;
+        ArrayList<Boolean> uppers = new ArrayList<Boolean>();
         for (int i=0; i<text.length(); i++) {
             if (Character.isUpperCase(text.charAt(i))) {
                 uppers.add(true);
@@ -46,13 +62,34 @@ class Scratch {
         }
         return result;
     }
-
-    public static void main(String[] args) {
-        String text = "ToapfihSDUAI  aid";
-        System.out.println(text.toUpperCase());
-        System.out.println(text.toLowerCase());
-        System.out.println(capitalize(text));
-        System.out.println(inverse(text));
-        
+    
+    public static String remove_duplicates(String text) {
+    	String result = "";
+    	text = text.trim();
+    	String[] tmp_list = text.split("\\s+");
+    	List<String> text_list = new ArrayList<String>();
+    	text_list = Arrays.asList(tmp_list);
+    	
+    	for (int i=0; i<text_list.size()-1; i++) {
+    		if (!text_list.get(i).equals(text_list.get(i+1))) {
+    			result = result + " " + text_list.get(i);
+    		}
+    	}
+    	if (!text_list.get(text_list.size() - 1).equals(text_list.size() - 2)) {
+			result = result + " " + text_list.get(text_list.size() - 1);
+		}
+    	return result.trim();
     }
+
+    
+    
+	public static void main(String[] args) {
+		String text = "To jest jest jest jest  przykladowe    zdanie";
+		System.out.println("Oryginalny tekst:		" + text);
+        System.out.println("Wszystko z dużych liter:	" + text.toUpperCase());
+        System.out.println("Wszystko z małych liter:	" + text.toLowerCase());
+        System.out.println("Duże pierwsze litery:		" + capitalize(text));
+        System.out.println("Odwrócony:			" + inverse(text));
+        System.out.println("Usunięte duplikaty:		" + remove_duplicates(text));
+	}
 }
