a3a9ee5e7225edc6e8ffe6312f423981337012de
Yorlock <dominikluk2@gmail.com>
2021-12-22 18:46:00 +0100

    Update TextTransformerController.java

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -15,19 +15,6 @@ public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(value ="/api/{text}", method = RequestMethod.GET, produces = "application/json")
-    public String gettest(@PathVariable String text,
-                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
-
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return  transformer.transform(text);
-    }
-
     @RequestMapping(value = "/api", method = RequestMethod.GET, produces = "application/json")
     public String get(@RequestBody TextDTO textDTO) throws JSONException {
 
