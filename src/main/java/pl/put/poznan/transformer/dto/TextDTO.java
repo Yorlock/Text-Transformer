9c308a4ea228a4c45207623ffe95c8aeecbfd31c
Yorlock <dominikluk2@gmail.com>
2021-12-22 14:55:28 +0100

    Small change.

--- src/main/java/pl/put/poznan/transformer/dto/TextDTO.java
+++ src/main/java/pl/put/poznan/transformer/dto/TextDTO.java
@@ -1,20 +1,18 @@
 package pl.put.poznan.transformer.dto;
 
+import java.util.List;
+
 public class TextDTO {
 
-    private String Text;
+    private String text;
+    private List<String> transforms;
 
-    public TextDTO(String text) {
-        Text = text;
+    public String[] getTransforms() {
+        return transforms.toArray(new String[0]);
     }
-
 
     public String getText() {
-        return Text;
-    }
-
-    public void setText(String text) {
-        Text = text;
+        return text;
     }
 
 }
--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -1,4 +1,6 @@
 package pl.put.poznan.transformer.rest;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -9,14 +11,12 @@ import java.util.Arrays;
 
 
 @RestController
-@RequestMapping("/api/{text}")
-@ResponseBody
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public TextDTO get(@PathVariable String text,
+    @RequestMapping(value ="/api/{text}", method = RequestMethod.GET, produces = "application/json")
+    public String gettest(@PathVariable String text,
                               @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
 
         // log the parameters
@@ -25,7 +25,23 @@ public class TextTransformerController {
 
         // perform the transformation, you should run your logic here, below is just a silly example
         TextTransformer transformer = new TextTransformer(transforms);
-        return new TextDTO(transformer.transform(text));
+        return  transformer.transform(text);
+    }
+
+    @RequestMapping(value = "/api", method = RequestMethod.GET, produces = "application/json")
+    public String get(@RequestBody TextDTO textDTO) throws JSONException {
+
+        //return textDTO.getText();
+        // log the parameters
+        logger.debug(textDTO.getText());
+        logger.debug(Arrays.toString(textDTO.getTransforms()));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        JSONObject response = new JSONObject();
+        TextTransformer textTransformer = new TextTransformer(textDTO.getTransforms());
+        response.put("text", textTransformer.transform(textDTO.getText()));
+
+        return response.toString();
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
@@ -40,9 +56,6 @@ public class TextTransformerController {
         TextTransformer transformer = new TextTransformer(transforms);
         return transformer.transform(text);
     }
-
-
-
 }
 
 
