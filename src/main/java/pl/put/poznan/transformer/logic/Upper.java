ebd87690e3362765561b165d58df7ec6f1460f6f
Anukia <59363243+Anukia@users.noreply.github.com>
2021-12-22 15:50:04 +0100

    Added Upper class Javadoc

--- src/main/java/pl/put/poznan/transformer/logic/Upper.java
+++ src/main/java/pl/put/poznan/transformer/logic/Upper.java
@@ -1,10 +1,21 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * This is a concrete decorator which converts all lowercase characters to uppercase
+ */
 class Upper extends TextDecorator {
+    /**
+     * Class constructor
+     * @param text String which we want to transform
+     */
     public Upper(String text) {
         super(text);
     }
 
+    /**
+     * converts all lowercase characters to uppercase
+     * @return the uppercased string
+     */
     @Override
     public String getText(){
         return super.getText().toUpperCase();}
