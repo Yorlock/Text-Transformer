bbdb016266812913a552a37b394d7d880883272f
Anukia <59363243+Anukia@users.noreply.github.com>
2021-12-22 11:39:53 +0100

    Added javadoc documentation in 2 classes

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -3,10 +3,16 @@ package pl.put.poznan.transformer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * This is class that starts whole appllication.
+ */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
 public class TextTransformerApplication {
 
+    /**
+     * Start the program with given parameters
+     * @param args array of parameters for the program
+     */
     public static void main(String[] args) {
         SpringApplication.run(TextTransformerApplication.class, args);
     }
--- src/main/java/pl/put/poznan/transformer/logic/Capitalize.java
+++ src/main/java/pl/put/poznan/transformer/logic/Capitalize.java
@@ -2,11 +2,23 @@ package pl.put.poznan.transformer.logic;
 
 import org.apache.commons.lang.WordUtils;
 
+/**
+ * This is a concrete decorator which capitalize given text
+ */
+
 class Capitalize extends TextDecorator {
+    /**
+     * class constructor
+     * @param text string which we want to transform
+     */
     public Capitalize(String text) {
         super(text);
     }
 
+    /**
+     * Converts all words in a text into capitalized words
+     * @return transformed text
+     */
     @Override
     public String getText(){
         return WordUtils.capitalizeFully(super.getText());}
